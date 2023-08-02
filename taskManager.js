df86e57cdaa80624e61835bf69e2c28cf8bb4724
unknown <juanicolas99@gmail.com>
2023-08-02 12:50:21 -0500

    segundo cambio

--- taskManager.js
+++ taskManager.js
@@ -1,20 +1,5 @@
 const fs = require('fs');
-const { addTask, deleteTask, completeTask } = require('./taskManager');
 
-
-function getRandomNumber() {
-  return new Promise((resolve, reject) => {
-    const randomNumber = Math.floor(Math.random() * 10);
-
-    setTimeout(() => {
-      resolve(randomNumber);
-    }, 3000);
-  });
-}
-
-module.exports = getRandomNumber;
-
-// Ahora, también implementaremos las funciones de la lista de tareas con promesas
 function addTask(task) {
   return new Promise((resolve, reject) => {
     fs.readFile('tasks.json', 'utf8', (err, data) => {
