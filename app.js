bbc1306d71e75af7a968bc34f18f79ee2bbfab6e
unknown <juanicolas99@gmail.com>
2023-08-19 09:23:46 -0500

    Agregando

--- app.js
+++ app.js
@@ -1,89 +1,38 @@
 const fs = require('fs');
 const { addTask, deleteTask, completeTask } = require('./taskManager');
 
+// Crear una nueva tarea y agregarla
+const newTask = {
+  description: 'Buy groceries',
+  completed: false
+};
 
-function getRandomNumber() {
-  return new Promise((resolve, reject) => {
-    const randomNumber = Math.floor(Math.random() * 10);
-
-    setTimeout(() => {
-      resolve(randomNumber);
-    }, 3000);
+addTask(newTask)
+  .then(result => {
+    console.log(result); // Debería imprimir "Task added successfully!"
+  })
+  .catch(error => {
+    console.error(error);
   });
-}
 
-module.exports = getRandomNumber;
+// Eliminar una tarea por índice
+const taskIndexToDelete = 0; // Por ejemplo, eliminar la primera tarea
 
-// Ahora, también implementaremos las funciones de la lista de tareas con promesas
-function addTask(task) {
-  return new Promise((resolve, reject) => {
-    fs.readFile('tasks.json', 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const tasks = JSON.parse(data);
-        tasks.push(task);
-
-        fs.writeFile('tasks.json', JSON.stringify(tasks), (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve('Task added successfully!');
-          }
-        });
-      }
-    });
+deleteTask(taskIndexToDelete)
+  .then(result => {
+    console.log(result); // Debería imprimir "Task deleted successfully!"
+  })
+  .catch(error => {
+    console.error(error);
   });
-}
 
-function deleteTask(index) {
-  return new Promise((resolve, reject) => {
-    fs.readFile('tasks.json', 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const tasks = JSON.parse(data);
-        if (index >= 0 && index < tasks.length) {
-          tasks.splice(index, 1);
+// Marcar una tarea como completada por índice
+const taskIndexToComplete = 1; // Por ejemplo, marcar la segunda tarea como completada
 
-          fs.writeFile('tasks.json', JSON.stringify(tasks), (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve('Task deleted successfully!');
-            }
-          });
-        } else {
-          reject(new Error('Invalid task index!'));
-        }
-      }
-    });
+completeTask(taskIndexToComplete)
+  .then(result => {
+    console.log(result); // Debería imprimir "Task marked as completed!"
+  })
+  .catch(error => {
+    console.error(error);
   });
-}
-
-function completeTask(index) {
-  return new Promise((resolve, reject) => {
-    fs.readFile('tasks.json', 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const tasks = JSON.parse(data);
-        if (index >= 0 && index < tasks.length) {
-          tasks[index].completed = true;
-
-          fs.writeFile('tasks.json', JSON.stringify(tasks), (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve('Task marked as completed!');
-            }
-          });
-        } else {
-          reject(new Error('Invalid task index!'));
-        }
-      }
-    });
-  });
-}
-
-module.exports = { addTask, deleteTask, completeTask };
--- taskManager.js
+++ taskManager.js
@@ -1,18 +1,22 @@
-const fs = require('fs');
-
-function addTask(task) {
+function addTask(description) {
   return new Promise((resolve, reject) => {
     fs.readFile('tasks.json', 'utf8', (err, data) => {
       if (err) {
         reject(err);
       } else {
         const tasks = JSON.parse(data);
-        tasks.push(task);
+        const newTask = {
+          id: tasks.length + 1, // Incrementa el id para cada nueva tarea
+          description: description,
+          completed: false
+        };
+        tasks.push(newTask);
 
         fs.writeFile('tasks.json', JSON.stringify(tasks), (err) => {
           if (err) {
             reject(err);
           } else {
+            console.log('New task added:', newTask); // Imprime la nueva tarea en la consola
             resolve('Task added successfully!');
           }
         });
@@ -20,55 +24,3 @@ function addTask(task) {
     });
   });
 }
-
-function deleteTask(index) {
-  return new Promise((resolve, reject) => {
-    fs.readFile('tasks.json', 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const tasks = JSON.parse(data);
-        if (index >= 0 && index < tasks.length) {
-          tasks.splice(index, 1);
-
-          fs.writeFile('tasks.json', JSON.stringify(tasks), (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve('Task deleted successfully!');
-            }
-          });
-        } else {
-          reject(new Error('Invalid task index!'));
-        }
-      }
-    });
-  });
-}
-
-function completeTask(index) {
-  return new Promise((resolve, reject) => {
-    fs.readFile('tasks.json', 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const tasks = JSON.parse(data);
-        if (index >= 0 && index < tasks.length) {
-          tasks[index].completed = true;
-
-          fs.writeFile('tasks.json', JSON.stringify(tasks), (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve('Task marked as completed!');
-            }
-          });
-        } else {
-          reject(new Error('Invalid task index!'));
-        }
-      }
-    });
-  });
-}
-
-module.exports = { addTask, deleteTask, completeTask };
